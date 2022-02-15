0a96566aab514923862b70f58377b1bb87c048a3
NachoPal <ignacio.palacios.santos@gmail.com>
2022-02-15 17:51:16 +0100

    query variables moved to variables

--- tests/assets/custom_assert.ts
+++ tests/assets/custom_assert.ts
@@ -2,7 +2,7 @@ const chai = require('chai');
 var should = require('chai').should()
 
 const myAssert = async (context, tab, ...args) => {
-  console.log(`${tab}Custom assert`)
+  console.log(`${tab}Custom assert ${JSON.stringify(args)}`)
 
   chai.assert.equal(true, true)
 
