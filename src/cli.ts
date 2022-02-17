e3e683204c0d5bada5958d67550b37a0e0c07f7f
NachoPal <ignacio.palacios.santos@gmail.com>
2022-02-17 14:56:17 +0100

    scripts and cli improved

--- src/cli.ts
+++ src/cli.ts
@@ -1,31 +1,17 @@
 import { spawn } from 'child_process';
-import { Command } from 'commander';
+import { Command, Option } from 'commander';
 const program = new Command();
 
-const main = async () => {
-  program
-  .name('parachains-integrations-tests')
-  .description('Tool for testing integration between Parachains')
-  .version('1.0.0');
+const spawnPolkadotLaunch = (options) => {
+  spawn(
+    "./node_modules/.bin/polkadot-launch", 
+    [options.config], {
+    stdio: 'inherit',
+    detached: false,
+  });
+}
 
-  program
-    .option('-c, --config <path>', 'path to polkadot-launch config file')
-    .requiredOption('-t, --tests <path>', 'path to tests')
-    .option('-to, --timeout <time>', 'timeout for tests');
-
-  program.parse(process.argv);
-
-  const options = program.opts();
-
-  if (options.config) {
-    spawn(
-      "./node_modules/.bin/polkadot-launch", 
-      [options.config], {
-      stdio: 'inherit',
-      detached: false,
-    });
-  }
-
+const spawnTests = (options) => {
   spawn(
     "./node_modules/.bin/mocha", 
     ['--timeout 100000', '--exit', '-r', 'ts-node/register', 'src/run.ts'], {
@@ -38,6 +24,45 @@ const main = async () => {
       TIMEOUT: options.timeout ? options.timeout : 100000
     }
   });
+}
+
+const main = async () => {
+  program
+  .name('parachains-integrations-tests')
+  .description('Tool for testing integration between Parachains')
+  .version('1.0.0');
+
+  program
+    .addOption(new Option('-m, --mode <mode>', 'polkadot-launch, test or both').choices(['polkadot-launch', 'test','polkadot-launch-test']).makeOptionMandatory())
+    .addOption(new Option('-c, --config <path>', 'path to polkadot-launch config file'))
+    .addOption(new Option('-t, --tests <path>', 'path to tests'))
+  
+  program.parse(process.argv);
+  let options = program.opts();
+
+  if (options.mode === 'polkadot-launch-test') {
+    program
+      .addOption(new Option('-c, --config <path>', 'path to polkadot-launch config file').makeOptionMandatory())
+      .addOption(new Option('-t, --tests <path>', 'path to tests').makeOptionMandatory())
+    
+    program.parse(process.argv);
+    spawnPolkadotLaunch(options)
+    spawnTests(options)
+
+  } else if (options.mode === 'polkadot-launch') {
+    program
+      .addOption(new Option('-c, --config <path>', 'path to polkadot-launch config file').makeOptionMandatory())
+    
+    program.parse(process.argv);
+    spawnPolkadotLaunch(options)
+
+  } else if (options.mode === 'test') {
+    program
+      .addOption(new Option('-t, --tests <path>', 'path to tests').makeOptionMandatory())
+    
+    program.parse(process.argv);
+    spawnTests(options)
+  }
 }
 
 main()
--- stop.sh
+++ stop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-killall -9 polkadot
-killall -9 polkadot-collator
-killall -9 parachain-collator
+# killall -9 polkadot
+# killall -9 polkadot-collator
+# killall -9 parachain-collator
+# killall -9 node
+
+pkill -f polkadot
+pkill -f parachain
